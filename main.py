b9c37eab74f30ca768749ce656409b0ca0991d7f
S1mna41S <g-id93@mail.ru>
2020-11-23 14:19:34 +0500

    слегка зафлпшил, чтобы глаза не мозолило

--- main.py
+++ main.py
@@ -11,19 +11,25 @@ def get_path_to_picture(picture_name):
     return path.normpath(path.join(PICTURES_FOLD, picture_name))
 
 
-image = cv.imread(get_path_to_picture('probe_pic1.jpg'), cv.IMREAD_GRAYSCALE)
-rows, cols = image.shape
-
-img = image.astype(np.float32)
-
-value = np.sqrt(((img.shape[0]/2.0)**2.0)+((img.shape[1]/2.0)**2.0))
-
-polar_image = cv.linearPolar(img,(img.shape[0]/2, img.shape[1]/2), value, cv.WARP_FILL_OUTLIERS)
+def read_image(picture_name):
+    image = cv.imread(get_path_to_picture('probe_pic1.jpg'), cv.IMREAD_GRAYSCALE)
+    rows, cols = image.shape
+    img = image.astype(np.float32)
+    return image, rows, cols
 
 
-polar_image = polar_image.astype(np.uint8)
-cv.imshow("Polar Image", polar_image)
-cv.imshow("Image", image)
+def varp_polar(img):
+    value = np.sqrt(((img.shape[0] / 2.0) ** 2.0) + ((img.shape[1] / 2.0) ** 2.0))
+    polar_image = cv.linearPolar(img, (img.shape[0] / 2, img.shape[1] / 2), value, cv.WARP_FILL_OUTLIERS)
+    return polar_image.astype(np.uint8)
 
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    image, rows, cols = read_image('probe_pic1.jpg')
+    polar_image = varp_polar(image)
+    
+    cv.imshow("Image", image)
+    cv.imshow("Polar Image", polar_image)
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
